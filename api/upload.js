78b87c5e41a646eb9bac928d7c43530d50372687
marconi-dm <marconidamascenomd@gmail.com>
2024-11-13 00:55:16 -0300

    fix vercel deploy

--- api/upload.js
+++ api/upload.js
@@ -4,7 +4,7 @@ const multer = require('multer');
 const path = require('path');
 const fs = require('fs');
 // Importa a função de tratamento de dados
-const processExcel = require('./data/data_processing');
+const processExcel = require('../data/data_processing');
 
 const app = express();
 const PORT = process.env.PORT || 3000;
